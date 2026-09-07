64936787872b5a1e858f03c297e81ec239409933
aranega <vincent.aranega@gmail.com>
2026-09-07 07:17:45 -0600

    Remove dead api for OrbitControl

--- src/controls/controls.ts
+++ src/controls/controls.ts
@@ -1,10 +1,8 @@
-import ControlsOrbit, { orbit } from './controls.orbit';
 import ControlsTrackball, { trackball } from './controls.trackball';
 import TrackballOrtho, { trackballOrtho } from './controls.trackballortho';
 
 export {
-    ControlsOrbit as OrbitControl,
-    orbit as orbitControlFactory, ControlsTrackball as TrackballControl,
+    ControlsTrackball as TrackballControl,
     trackball as trackballControlFactory,
     TrackballOrtho as TrackballOrthoControl,
     trackballOrtho as trackballOrthoControlFactory
